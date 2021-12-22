5c8f4a003f479859a25b48181d8968b79cb939fa
MrRoy121 <nilashishroyjoy@gmail.com>
2021-12-22 20:46:29 +0600

    Day 01

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome To Catalog"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
